5b98bb3eef448b0a507d8ada86002d46af80c8fb
JulieBodette <jr3python@gmail.com>
2024-01-27 18:33:26 -0500

    added win scene and code to load win scene

--- level1.gd
+++ level1.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+func Win():
+	print("Win")
+	#this code is janky
+	#see https://github.com/godotengine/godot/issues/85251
+	if(is_inside_tree()):
+		get_tree().change_scene_to_file("res://win.tscn")
+	else:
+		print("error when attempting to load win scene")
+	
 func GameOver():
 	print("Game Over")
 	#this code is janky

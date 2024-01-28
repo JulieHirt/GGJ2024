a8b5ddd5b778561e6e130be0e74d0f115bde17b9
Isaac Phillips <ip657318@ohio.edu>
2024-01-28 14:08:06 -0500

    Subtitles on the prologue & Level music

--- level1.gd
+++ level1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+func _ready():
+	$ClownSneak.play()
 
 func Win():
 	print("Win")
--- prologue_dialogue.gd
+++ prologue_dialogue.gd
@@ -5,6 +5,7 @@ var phase = 0
 
 func _ready():
 	$WhoYou.play()
+	$TextEdit.text = "Huh? Who are you?"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,10 +16,12 @@ func _process(delta):
 				t = 2.5
 				phase = 1
 				$MyName.play()
+				$TextEdit.text = "Hello, my name is Bubbles."
 			1:
 				t = 11.5
 				phase = 2
 				$HelloMister.play()
+				$TextEdit.text = "Hello mister Bubbles. You have been assigned to assasinate the bad man, Mr. Man. Carry out this task or you will be terminated forever. Good luck. Have fun."
 			2:
 				get_tree().change_scene_to_file("res://level1.tscn")
 	else:

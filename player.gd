5156e0348faff308b632d7a43c6c4c47f130b5d2
JulieBodette <jr3python@gmail.com>
2024-01-28 18:06:59 -0500

    updated walking code- works correctly

--- player.gd
+++ player.gd
@@ -2,15 +2,17 @@ extends CharacterBody2D
 
 @onready var animation = get_node("AnimatedSprite2D")
 
+var anim_done = 0
+var input_direction
+
 const speed = 300.0
 
 func _ready():
 	animation.play("Idle")
 	$HelloSFX.play()
-
-func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_direction * speed
+	
+func update_walk_anim():
+	#set the correct animation based on the direction the player is facing
 	if input_direction:
 		if input_direction.x > 0:
 			animation.play("Walk Right")
@@ -29,5 +31,9 @@ func get_input():
 	
 
 func _physics_process(_delta):
-	get_input()
+	#get the directional key that the player is pressing to move
+	input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	#set the velocity so that the player will move in correct direction
+	velocity = input_direction * speed
+	update_walk_anim()
 	move_and_slide()

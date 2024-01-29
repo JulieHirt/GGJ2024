92f7a966779f7bbf3af69be694308aef36777ba8
JulieBodette <jr3python@gmail.com>
2024-01-28 19:00:50 -0500

    holy fudge it works
    never touch this again
    
    learn to use an animation tree next time

--- player.gd
+++ player.gd
@@ -12,6 +12,7 @@ func _ready():
 	$HelloSFX.play()
 	
 func update_walk_anim():
+	input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	#set the correct animation based on the direction the player is facing
 	if input_direction:
 		if input_direction.x > 0:
@@ -23,17 +24,33 @@ func update_walk_anim():
 		if input_direction.y < 0:
 			animation.play("Walk Up")
 	else:
-		animation.play("Idle")
-	#Hat Action is defined as the "E" Key on the keyboard
-	if Input.is_action_just_pressed("Hat Action"):
-		#animation.play("Hat") //we don't have this defined yet
-		pass
-	
+		if Input.is_action_just_pressed("Hat Action"):
+			animation.stop()
+			animation.play("Hat")
+		else:
+			animation.play("Idle")	
 
-func _physics_process(_delta):
+func _process(_delta):
+	#########move the player############
 	#get the directional key that the player is pressing to move
 	input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	#set the velocity so that the player will move in correct direction
 	velocity = input_direction * speed
-	update_walk_anim()
 	move_and_slide()
+	#################play the correct animation##########
+	
+	#Hat Action is defined as the "E" Key on the keyboard
+	if Input.is_action_just_pressed("Hat Action"):
+		animation.stop()
+		animation.play("Hat")
+		anim_done = 0
+	if anim_done == 1:
+		update_walk_anim()
+	#anim_done = 0
+
+
+
+func _on_animated_sprite_2d_animation_finished():
+	anim_done = 1
+	#update_walk_anim()
+	animation.play("Idle")
